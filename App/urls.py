3b2b254a4413f945567028ccc757a1305179505b
SreeSathwika <165075718+SreeSathwika@users.noreply.github.com>
2024-04-19 14:17:45 +0530

    Update urls.py

--- App/urls.py
+++ App/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',views.login,name='login'),  
+    path('register/',views.register,name='register'), 
+    path('index/', views.index, name='index'),
     path('video_feed/', views.video_feed, name='video_feed'), 
 ]
